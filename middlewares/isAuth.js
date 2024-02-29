f8564427a2a5db2bd8850abfe36c08adb4962aa4
Amir <fallahiamirmohammad@gmail.com>
2024-02-29 23:54:15 +0330

    move auth to seprate file and support cookie

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,8 +1,24 @@
-module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect("/login");
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+    res.locals.isAuthenticated = false;
+    if (req.session.isLoggedIn) {
+        res.locals.isAuthenticated = req.session.isLoggedIn ? req.session.isLoggedIn : false;
+    } else {
+        const token = req.cookies.thedano;
+        if (token) {
+          const decoded = jwt.verify(token, process.env.jwtToken);
+          if (decoded.token) {
+            let [isValid, user] = await User.byToken({token: decoded.token});
+            if (isValid) {
+                req.session.isLoggedIn = true;
+                req.session.user = user;
+                res.locals.isAuthenticated = true;
+            }
+          }
+        }
     }
-    res.locals.isLoggedIn = req.session.isLoggedIn
-    res.locals.user = req.session.user
+    console.log(res.locals.isAuthenticated, req.session.isLoggedIn)
     next();
 };
