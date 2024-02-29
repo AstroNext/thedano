8c4d4d264b1928b4f8f1c52c30930e2daaa38742
Amir <fallahiamirmohammad@gmail.com>
2024-03-01 01:00:53 +0330

    added dashboard route

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var authMiddleware = require('./middlewares/isAuth');
 
 var indexRouter = require('./routes/home');
 var productRouter = require('./routes/product');
+var dashboardRouter = require('./routes/dashboard');
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
 var adminRouter = require('./routes/admin');
@@ -26,6 +27,8 @@ app.set('view engine', 'ejs');
 // page configration
 app.set('home', { render: '../views/pages/home', title: 'Home'});
 app.set('product', { render: '../views/pages/product', title: 'Product'});
+app.set('dashboard', { render: '../views/pages/dashboard', panel: '../dashboard/main', title: 'Dashboard'});
+app.set('orders', { render: '../views/pages/dashboard', panel: '../dashboard/orders', title: 'Orders'});
 app.set('login', { render: '../views/pages/login', title: 'Login'});
 app.set('signup', { render: '../views/pages/signup', title: 'Sign up'});
 app.set('admin', { render: '../views/pages/admin', title: 'Admin'});
@@ -46,6 +49,7 @@ app.use(authMiddleware);
 
 app.use(indexRouter);
 app.use(productRouter);
+app.use(dashboardRouter);
 app.use(loginRouter);
 app.use(signupRouter);
 app.use(adminRouter);
