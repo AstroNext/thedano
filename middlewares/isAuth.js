ad0116632bf02816b1b6e9777139d0e8c476367a
Amir <fallahiamirmohammad@gmail.com>
2024-03-01 20:29:31 +0330

    hoooffff agha adib

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -2,23 +2,22 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-    res.locals.isAuthenticated = false;
-    if (req.session.isLoggedIn) {
-        res.locals.isAuthenticated = req.session.isLoggedIn ? req.session.isLoggedIn : false;
-    } else {
-        const token = req.cookies.thedano;
-        if (token) {
-          const decoded = jwt.verify(token, process.env.jwtToken);
-          if (decoded.token) {
-            let [isValid, user] = await User.byToken({token: decoded.token});
-            if (isValid) {
-                req.session.isLoggedIn = true;
-                req.session.user = user;
-                res.locals.isAuthenticated = true;
-            }
+  res.locals.isAuthenticated = false;
+  if (req.session.isLoggedIn) {
+      res.locals.isAuthenticated = req.session.isLoggedIn ? req.session.isLoggedIn : false;
+  } else {
+      const token = req.cookies.thedano;
+      if (token) {
+        const decoded = jwt.verify(token, process.env.jwtToken);
+        if (decoded.token) {
+          let [isValid, user] = await User.byToken({token: decoded.token});
+          if (isValid) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.locals.isAuthenticated = true;
           }
         }
-    }
-    console.log(res.locals.isAuthenticated, req.session.isLoggedIn)
-    next();
+      }
+  }
+  next();
 };
